d86adfb77928ff2ae0c9641c91fe8925fd108357
antonioarjpi <antonioarjpi@gmail.com>
2022-01-07 21:15:44 -0300

    Footer - nav e home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,17 +5,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+import { MatSidenavModule }from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
+
+import { MatCard, MatCardModule }from '@angular/material/card'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
